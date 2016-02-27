fdf14359541019647e554c26bff63bc82582aea6
Kiko <mmhabib11@gmail.com>
2016-02-27 09:11:40 -0500

    Cube portfolio

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
-//= require jquery
+//= require jquery.min
 //= require jquery_ujs
 //= require materialize-sprockets
 //= require homes
+//= require jquery.cubeportfolio.min
+//= require cube-portfolio
