94a92798c4ccdfb332916144eff29251b4195594
Kiko <mmhabib11@gmail.com>
2016-02-27 09:46:26 -0500

    Category and show page

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Item do
 
-permit_params :title, :image, :description, :price
+	permit_params :title, :image, :description, :price, :category
 
+	form do |f|
+		f.inputs "Items" do
+			f.input :title
+			f.input :description
+			f.input :price
+			f.input :category, :label => 'Category', :as => :select, :collection => ['weddings', 'birthdays', 'custom']
+			f.input :image
+		end
+		f.actions
+	end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ActiveRecord::Base
 	mount_uploader :image, ItemUploader
+
+	def wedding_category?
+		category === "weddings"
+	end
+
+	def birthday_category?
+		category === "birthdays"
+	end
+
+	def custom_category?
+		category === "custom"
+	end
 end
