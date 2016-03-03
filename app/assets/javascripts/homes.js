7f5f9f72b4596c27514edf4cae879886ea1efedb
Kiko <mmhabib11@gmail.com>
2016-03-03 16:08:03 -0500

    Scrolling and styling bug fixes

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
 	$('.modal-trigger').leanModal();
 	$('.carousel').carousel();
 	$('.button-collapse').sideNav();
+
+	$('.navbar-fixed li a').click(function(e){
+		e.preventDefault();
+		var id = $(this).attr('href');
+
+		if (id === "#aboutme") {
+			$('html, body').animate({
+				scrollTop: 0
+			}, 1000);
+		}else {
+			$('html, body').animate({
+				scrollTop: $(id).offset().top - 60
+			}, 1000);
+		}
+	});
+
 });
