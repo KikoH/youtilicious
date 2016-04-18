fa9f49129961d02ca07da3c013de4c65c54f4770
Kiko <mmhabib11@gmail.com>
2016-04-18 16:18:32 -0400

    Image size limit increase and typo fix

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ActiveRecord::Base
 	belongs_to :item
 	mount_uploader :image, ItemUploader
 
-	validates :image, file_size: { less_than_or_equal_to: 0.5.megabytes.to_i }
+	validates :image, file_size: { less_than_or_equal_to: 1.megabytes.to_i }
 end
